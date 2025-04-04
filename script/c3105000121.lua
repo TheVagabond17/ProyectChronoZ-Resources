5e840ddd2f37779a7670424f496904768c44dd1a
TheVagabond17 <yugiohchronoz@gmail.com>
2025-04-03 22:39:15 -0400

    Add files via upload

--- script/c3105000121.lua
+++ script/c3105000121.lua
@@ -84,16 +84,16 @@ end
 
 function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
 	if chk == 0 then
-		local g = Duel.GetMatchingGroup(s.diffnamefilter, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE, 0, nil, e, tp)
+		local g = Duel.GetMatchingGroup(s.diffnamefilter, tp, LOCATION_HAND + LOCATION_DECK, 0, nil, e, tp)
 		return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and #g > 0
 	end
-	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND + LOCATION_DECK)
 end
 
 function s.spop(e, tp, eg, ep, ev, re, r, rp)
 	if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
 	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-	local g = Duel.SelectMatchingCard(tp, s.diffnamefilter, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil, e, tp)
+	local g = Duel.SelectMatchingCard(tp, s.diffnamefilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, 1, nil, e, tp)
 	if #g > 0 then
 		Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
 	end
