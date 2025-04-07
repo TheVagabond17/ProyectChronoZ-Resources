60c40f97c8ff09675b728b3a0f2b3325910a42ec
TheVagabond17 <yugiohchronoz@gmail.com>
2025-04-06 20:03:32 -0400

    Add files via upload

--- script/c3105000129.lua
+++ script/c3105000129.lua
@@ -1,10 +1,10 @@
 -- D.D. Soldier
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Efecto 1: Si esta carta es desterrada, destruir 1 carta del oponente
+	-- Efecto 1: Si esta carta es desterrada, desterrar temporalmente una carta del oponente
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_REMOVE)
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 
-	-- Efecto 2: Si es Invocado de Modo Especial desde el Destierro, añadir "Dimensional Fissure"
+	-- Efecto 2: Si es Invocado desde el Destierro y no controlas Dimensional Fissure, añade 1 desde el Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -27,34 +27,66 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
--- Efecto 1: Destruir 1 carta del oponente cuando esta carta es desterrada
+-- Efecto 1: Desterrar temporalmente una carta del oponente
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
-	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
+
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.Destroy(tc,REASON_EFFECT)
+	if not tc or not tc:IsRelateToEffect(e) then return end
+
+	local pos = tc:IsFacedown() and POS_FACEDOWN or POS_FACEUP
+	local p = tc:GetControler()
+
+	if Duel.Remove(tc,pos,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,pos)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetLabelObject(tc)
+		e1:SetLabel(p)
+		e1:SetCondition(s.retcon)
+		e1:SetOperation(s.retop)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
 end
 
--- Efecto 2: Verificar si fue Invocado de Modo Especial desde el Destierro
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_REMOVED)
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	return tc and tc:GetFlagEffect(id)>0
 end
 
--- Efecto 2: Buscar "Dimensional Fissure"
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	local pos=tc:GetFlagEffectLabel(id)
+	if not tc then return end
+	Duel.ReturnToField(tc)
+	if pos==POS_FACEDOWN then
+		Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)
+	end
+end
+
+-- Efecto 2: Verificar si fue Invocado de Modo Especial desde el Destierro y no controlas "Dimensional Fissure"
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_REMOVED) and not Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_ONFIELD,0,1,nil,81674782)
+end
+
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
+
 function s.thfilter(c)
-	return c:IsCode(81674782) and c:IsAbleToHand() -- "Dimensional Fissure"
+	return c:IsCode(81674782) and c:IsAbleToHand()
 end
+
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
@@ -63,4 +95,6 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
+
+
 
