85a1ed1b39b09e8e2727aea0553cf662874ef513
TheVagabond17 <yugiohchronoz@gmail.com>
2025-04-28 22:33:38 -0400

    Add files via upload

--- script/c2903000101.lua
+++ script/c2903000101.lua
@@ -1,15 +1,13 @@
 --Manipulation of the Dark Arts
 local s,id=GetID()
-s.listed_names={CARD_DARK_MAGICIAN} 
-
 function s.initial_effect(c)
-	-- Activar
+	c:SetUniqueOnField(1,0,id)
+	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id) -- Limitar a una vez por turno
+	e1:SetCountLimit(1,id)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
@@ -19,13 +17,8 @@ function s.initial_effect(c)
 end
 
 function s.costfilter(c)
-	return c:IsRace(RACE_SPELLCASTER) and (c:IsLevelBelow(6) or c:IsCode(46986414)) and c:IsAbleToGraveAsCost()
+	return (c:IsCode(46986414) or (c:IsRace(RACE_SPELLCASTER) and c:IsLevelBelow(6))) and c:IsAbleToGraveAsCost()
 end
-
-function s.thfilter(c)
-	return c:IsRace(RACE_SPELLCASTER) and c:IsAbleToHand() and c:IsLevelAbove(9)
-end
-
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
@@ -33,16 +26,32 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 
+function s.thfilter(c)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsLevelAbove(9) and c:IsAbleToHand()
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsLevelBelow(6) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then 
+		return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+	end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if g:GetCount()>0 then
+	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
+	end
+	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+			if #sg>0 then
+				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+			end
+		end
 	end
 end
