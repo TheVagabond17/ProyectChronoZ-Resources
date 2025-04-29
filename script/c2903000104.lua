8c41879260a3d3718f09abc96caed2b645dc99cd
TheVagabond17 <yugiohchronoz@gmail.com>
2025-04-28 23:50:02 -0400

    Add files via upload

--- script/c2903000104.lua
+++ script/c2903000104.lua
@@ -1,6 +1,6 @@
 --Summoned Skull - Darkness Summoner
 local s,id=GetID()
-s.listed_names={CARD_DARK_MAGICIAN, CARD_DARK_MAGICIAN_GIRL}
+s.listed_names={CARD_DARK_MAGICIAN}
 
 function s.initial_effect(c)
 	-- Change name to "Summoned Skull" in all zones, while keeping its own name
@@ -8,47 +8,35 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_ADD_CODE)
-	e1:SetValue(70781052) -- Código de Buster Blader
+	e1:SetValue(70781052) -- Código de Summoned Skull
 	c:RegisterEffect(e1)
 
 	-- Special Summon from hand
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,0))
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e5:SetType(EFFECT_TYPE_IGNITION)
-	e5:SetRange(LOCATION_HAND)
-	e5:SetCountLimit(1,id)
-	e5:SetCost(s.spcost)
-	e5:SetTarget(s.sptg)
-	e5:SetOperation(s.spop)
-	c:RegisterEffect(e5)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
 
-	-- Special Summon from GY during your turn
+	-- Quick effect to Special Summon from GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,{id,1})
-	e3:SetCondition(s.your_turn_cond)
-	e3:SetCost(s.your_turn_cost)
-	e3:SetTarget(s.your_turn_target)
-	e3:SetOperation(s.your_turn_operation)
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_MAIN_END)
+	e3:SetCondition(s.quick_condition)
+	e3:SetCost(s.quick_cost)
+	e3:SetTarget(s.quick_target)
+	e3:SetOperation(s.quick_operation)
 	c:RegisterEffect(e3)
-
-	-- Special Summon from GY during opponent's turn
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,2))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,{id,2})
-	e4:SetCondition(s.opponent_turn_cond)
-	e4:SetCost(s.opponent_turn_cost)
-	e4:SetTarget(s.opponent_turn_target)
-	e4:SetOperation(s.opponent_turn_operation)
-	c:RegisterEffect(e4)
 end
 
 function s.spcfilter(c)
@@ -75,50 +63,29 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.your_turn_cond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
+function s.quick_condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase()
 end
 
-function s.your_turn_cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.quick_cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 
-function s.your_turn_filter(c,e,tp)
-	return (c:IsCode(CARD_DARK_MAGICIAN) or c:IsCode(CARD_DARK_MAGICIAN_GIRL) or (c:IsRace(RACE_SPELLCASTER+RACE_WARRIOR+RACE_DRAGON) and c:IsLevelAbove(6))) 
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.quick_filter(c,e,tp)
+	return (c:IsCode(CARD_DARK_MAGICIAN) or c:ListsCode(CARD_DARK_MAGICIAN)) 
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
 end
 
-function s.your_turn_target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.your_turn_filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+function s.quick_target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsExistingMatchingCard(s.quick_filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 
-function s.your_turn_operation(e,tp,eg,ep,ev,re,r,rp)
+function s.quick_operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.your_turn_filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	end
-end
-
-function s.opponent_turn_cond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
-end
-
-function s.opponent_turn_cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
-end
-
-function s.opponent_turn_target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.your_turn_filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
-end
-
-function s.opponent_turn_operation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.your_turn_filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.quick_filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
