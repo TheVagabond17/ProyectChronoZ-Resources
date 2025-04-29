3085b9db6f241b7df0d9b8d1f8daa42aceea1c27
TheVagabond17 <yugiohchronoz@gmail.com>
2025-04-28 23:58:31 -0400

    Add files via upload

--- script/c2903000102.lua
+++ script/c2903000102.lua
@@ -1,11 +1,10 @@
---- Palladium Oracle - Buster Blader
+-- Palladium Oracle - Buster Blader
 local s,id=GetID()
 
--- Lista "Buster Blader" para reconocimiento de otras cartas
 s.listed_names={CARD_DARK_MAGICIAN, 78193831}
 
 function s.initial_effect(c)
-	-- Change name to "Buster Blader" in all zones, while keeping its own name
+	-- Change name to "Buster Blader" in all zones
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
@@ -14,31 +13,34 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	
 	-- Special Summon from hand
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_HAND)
-	e3:SetCountLimit(1,id)
-	e3:SetCost(s.spcost)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
-	c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
 	
-	-- Add Spellcaster, Warrior, or Dragon monster from Deck to hand
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e4:SetType(EFFECT_TYPE_IGNITION)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,{id,1})
-	e4:SetTarget(s.thtg)
-	e4:SetOperation(s.thop)
-	c:RegisterEffect(e4)
+	-- Add "Dark Magician" or Level 6+ monster that mentions "Dark Magician"
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
 end
 
+-- Cost for Special Summon
 function s.spcfilter(c)
-	return (c:IsRace(RACE_SPELLCASTER) or (c:IsType(TYPE_SPELL+TYPE_TRAP) and c:ListsCode(CARD_DARK_MAGICIAN) and (c:IsLocation(LOCATION_HAND) or c:IsFaceup()))) and c:IsAbleToGraveAsCost()
+	return (c:IsRace(RACE_SPELLCASTER) or (c:IsType(TYPE_SPELL+TYPE_TRAP) and c:ListsCode(CARD_DARK_MAGICIAN)))
+		and (c:IsLocation(LOCATION_HAND) or (c:IsFaceup() and c:IsLocation(LOCATION_MZONE+LOCATION_SZONE)))
+		and c:IsAbleToGraveAsCost()
 end
 
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -60,8 +62,10 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+-- Search
 function s.thfilter(c)
-	return (c:IsRace(RACE_SPELLCASTER) or c:IsRace(RACE_WARRIOR) or c:IsRace(RACE_DRAGON)) and (c:IsLevel(6) or c:IsLevel(7) or c:IsLevel(8)) and c:IsAbleToHand() and not c:IsCode(id)
+	return (c:IsCode(CARD_DARK_MAGICIAN) or (c:IsLevelAbove(6) and c:ListsCode(CARD_DARK_MAGICIAN)))
+		and c:IsAbleToHand()
 end
 
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -72,10 +76,9 @@ end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
+	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-
 
